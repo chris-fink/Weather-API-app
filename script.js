4ff5059b9d88ab30b2131369708c5f6551b4161c
Chris Fink <65560083+chris-fink@users.noreply.github.com>
2022-09-09 14:46:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 //API variables
-const url = 'https://api.openweathermap.org/data/2.5/weather?q={search}&appid={key}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=spartanburg&appid=7f520456e051f1332b94327cf4f33666&units=imperial';
 const key = '7f520456e051f1332b94327cf4f33666';
 const main = document.querySelector('main');
 const search = document.querySelector('#search');
 const form = document.querySelector('form');
 
+//Event Listener to allow user to search their location
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchTerm = search;
+    //Uncaught TypeError: Cannot read properties of null (reading 'value') at HTMLFormElement.<anonymous> (script.js:13:31)
+    if (searchTerm) {
+        searchLocation('https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid=' + key + '&units=imperial');
+        //URL should be https://api.openweathermap.org/data/2.5/weather?q=spartanburg&appid=7f520456e051f1332b94327cf4f33666&units=imperial
+        search.value = '';
+    }
+});
 
 //User search directs & updates API URL
 function searchLocation(url) {
-    fetch(url)
+    fetch(url, settings)
     .then(res => res.json())
-    .then(function(data) {
+    .then((res) => {
+        let city = res(data.name);
+        let temp = res(Math.round(data.main.temp));
+        let weather = res(data.weather.description);
+        let image = res(data.weather.icon);
+    })
+    .then(Log => {
+        console.log(city);
+        console.log(temp);
+        console.log(weather);
+        console.log(image);
+    })
 
         //Create HTML elements
         const img = document.createElement('img');
@@ -29,20 +53,6 @@ function searchLocation(url) {
                 h1.innerHTML = `${data.name}`;
                 h2.innerHTML = `${Math.round(data.main.temp)} °F`;
                 p.innerHTML = `${data.weather.description}`;
-    });
 }
 
-//Search form to allow user to search their location
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    main.innerHTML = '';
 
-    //Uncaught TypeError: Cannot read properties of null (reading 'value') at HTMLFormElement.<anonymous> (script.js:40:31)
-    const searchTerm = search.value;
-    if (searchTerm) {
-        searchURL('https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid=' + key + '&units=imperial');
-        //URL should be https://api.openweathermap.org/data/2.5/weather?q=spartanburg&appid=7f520456e051f1332b94327cf4f33666&units=imperial
-        search.value = '';
-    }
-        console.log
-});
