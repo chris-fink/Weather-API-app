aa8d57f01455c467589dd216fb39c65cb6bc56eb
Chris Fink <65560083+chris-fink@users.noreply.github.com>
2022-09-08 22:12:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,19 @@ function searchLocation(url) {
         const img = document.createElement('img');
         const h1 = document.createElement('h1');
         const h2 = document.createElement('h2');
+        const p = document.createElement('p');
 
             //Append elementsthe main elements
             main.appendChild(img);
             main.appendChild(h1);
             main.appendChild(h2);
+            main.appendChild(p);
             img.src = `${data.weather.icon}`;
 
                 //Display API data
-                h1.innerHTML = '';
-                h2.innerHTML = '';
+                h1.innerHTML = `${data.name}`;
+                h2.innerHTML = `${Math.round(data.main.temp)} °F`;
+                p.innerHTML = `${data.weather.description}`;
     });
 }
 
@@ -34,9 +37,12 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     main.innerHTML = '';
 
+    //Uncaught TypeError: Cannot read properties of null (reading 'value') at HTMLFormElement.<anonymous> (script.js:40:31)
     const searchTerm = search.value;
     if (searchTerm) {
-        searchURL('https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid=7f520456e051f1332b94327cf4f33666');
+        searchURL('https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + '&appid=' + key + '&units=imperial');
+        //URL should be https://api.openweathermap.org/data/2.5/weather?q=spartanburg&appid=7f520456e051f1332b94327cf4f33666&units=imperial
         search.value = '';
     }
+        console.log
 });
